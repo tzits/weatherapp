14e5d9741f37d794e231262b2e10f733cc6d02f0
Toby <tzitsman@gmail.com>
2023-02-07 13:01:45 -0500

    Update app.js

--- weather/app.js
+++ weather/app.js
@@ -1,6 +1,38 @@
-window.addEventListener('load', () => {
-    let long, lat;
+const findWeather = async () => {
+        let zipCode = input.value
 
+        if (zipCode.length == 5) {
+            let result = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${zipCode},US&key=${gmKey}`) 
+            let data = await result.json()
+            let {lat, lng} = data.results[0].geometry.location
+            const api = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${lat},${lng}&api=yes`
+            const getData = async(api) => {
+                const response = await fetch(api)
+                const data = await response.json()
+                console.log(data)
+                const {temp_c, temp_f, feelslike_c, feelslike_f, condition} = data.current
+                    let {text, icon} = condition
+
+                    let tz = `${data.location.name}, ${data.location.region}`
+
+                    setConditions(temp_f, text, tz, feelslike_f, icon)
+
+                    degreeSection.addEventListener('click', () => {
+                        if (degreeSpan.textContent === 'F') {
+                            changeUnits('C', temp_c, feelslike_c)
+                        } else {
+                            changeUnits('F', temp_f, feelslike_f)
+                        }
+                    })
+            }
+            getData(api)
+        }
+        else {
+            alert('Invalid ZipCode')
+        }
+    }
+
+    let long, lat;
     let temperatureDescription = document.querySelector('.temperature-description')
     let temperatureDegree = document.querySelector('.temperature-degree')
     let locationTimezone = document.querySelector('.location-timezone')
@@ -8,6 +40,10 @@ window.addEventListener('load', () => {
     let degreeSection = document.querySelector('.degree-section')
     let degreeSpan = document.querySelector('.cf')
     let realFeel = document.querySelector('.realfeel')
+    let submit = document.querySelector('.submit')
+    let input = document.querySelector('.zip')
+
+    submit.addEventListener('click', findWeather)
 
     const changeUnits = (unit, temp, feel) => {
         degreeSpan.textContent = unit;
@@ -18,41 +54,11 @@ window.addEventListener('load', () => {
     const setConditions = (temp, text, tz, feel, icon) => {
         temperatureDegree.textContent = temp;
         temperatureDescription.textContent = text;
-        locationTimezone.textContent = `Timezone = ${tz}`;
+        locationTimezone.textContent = tz;
         realFeel.textContent = `feels like ${feel}`
         icon = 'https:' + icon
         myIcon.src = icon;
         myIcon.setAttribute("style", "height: 100px; width: 100px")
     }
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude
-            lat = position.coords.latitude
-            const api = `http://api.weatherapi.com/v1/current.json?key=${key}&q=${lat},${long}&api=yes`
-            fetch(api)
-                .then(response => {
-                    return response.json()
-                })
-                .then(data => {
-                    const {temp_c, temp_f, feelslike_c, feelslike_f, condition} = data.current
-                    let {text, icon} = condition
 
-                    setConditions(temp_f, text, data.location.tz_id, feelslike_f, icon)
-
-                    degreeSection.addEventListener('click', () => {
-                        if (degreeSpan.textContent === 'F') {
-                            changeUnits('C', temp_c, feelslike_c)
-                        } else {
-                            changeUnits('F', temp_f, feelslike_f)
-
-                        }
-                    })
-
-                })
-        })
-    } else {
-        alert('not gonna work')
-    }
-
-})
